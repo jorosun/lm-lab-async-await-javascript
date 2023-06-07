c169ec37eee3e86d68291e1e106a1f843850df09
Jennifer Orosun <orosunj@gmail.com>
2023-06-07 23:26:31 +0100

    converted fetchData into async/await

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,20 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
+// const fetchData = (apiEndPoint) => {
+//   fetch(apiEndPoint)
+//     .then((response) => response.json())
+//     .then((json) => console.log(json))
+//     .catch((error) => console.log(error));
+
+const fetchData = async (apiEndPoint) => {
+  try {
+    const response = await fetch(apiEndPoint);
+    const json = await response.json();
+    console.log(json);
+  } catch (errorCode) {
+    console.log(response.errorCode);
+  }
 };
 
 fetchData(jsonTypicode);
