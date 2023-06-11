af3a360719cdd163471c76846e77ca9b50e6e17b
Jennifer Orosun <orosunj@gmail.com>
2023-06-11 14:05:41 +0100

    removed commented section

--- exercise002.js
+++ exercise002.js
@@ -2,12 +2,6 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-// const fetchData = (apiEndPoint) => {
-//   fetch(apiEndPoint)
-//     .then((response) => response.json())
-//     .then((json) => console.log(json))
-//     .catch((error) => console.log(error));
-
 const fetchData = async (apiEndPoint) => {
   try {
     const response = await fetch(apiEndPoint);
